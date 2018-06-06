052735b04f5bc154aa18feb97d811b2bd1ef4682
Lloyd Pick <lloydpick@gmail.com>
2018-06-06 14:48:40 -0400

    dependent destroy

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -9,15 +9,15 @@ class Alert < ApplicationRecord
   belongs_to :severity_internal, class_name: 'Severity'
   belongs_to :severity_external, class_name: 'Severity'
 
-  has_many :alert_departments
-  has_many :alert_users
+  has_many :alert_departments, dependent: :destroy
+  has_many :alert_users, dependent: :destroy
 
   has_many :departments, through: :alert_departments
   has_many :users, through: :alert_users
 
-  has_many :audits
-  has_many :urls
-  has_many :key_values
+  has_many :audits, dependent: :destroy
+  has_many :urls, dependent: :destroy
+  has_many :key_values, dependent: :destroy
 
   validates :state, presence: true
   validates :kind, presence: true
