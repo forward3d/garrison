7a35962ca964a532a4884082a5e0af32ee80caea
Lloyd Pick <lloydpick@gmail.com>
2018-06-07 13:53:04 -0400

    make the routes accurate

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :alerts, only: [:index, :create, :show, :update]
-      resources :sources, only: [:index, :create, :show, :update]
+      resources :alerts, only: [:index, :create, :show]
     end
   end
 end
