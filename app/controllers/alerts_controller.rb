7a496fcd47e1ff29066b5574ccfad56a8b5e62d8
Lloyd Pick <lloydpick@gmail.com>
2018-06-07 14:50:59 -0400

    show alerts

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,15 @@
 class AlertsController < ApplicationController
 
   def index
+    @alerts = Alert.kept.includes(:severity_internal).order('severities.order desc, last_detected_at desc')
+  end
+
+  def show
+    @alert = Alert.kept.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.js { render layout: false }
+    end
   end
 
 end
