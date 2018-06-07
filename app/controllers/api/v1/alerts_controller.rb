5c83ec28c277c1a1bfeec263ca3907ca28fb9e99
Lloyd Pick <lloydpick@gmail.com>
2018-06-07 13:52:37 -0400

    create alert api

--- app/controllers/api/v1/alerts_controller.rb
+++ app/controllers/api/v1/alerts_controller.rb
@@ -6,8 +6,65 @@ class Api::V1::AlertsController < Api::V1::BaseController
   end
 
   def show
-    user = Alert.kept.find(params[:id])
-    render json: user, serializer: Api::V1::AlertSerializer
+    alert = Alert.kept.find(params[:id])
+    render json: alert, serializer: Api::V1::AlertSerializer
+  end
+
+  def create
+    alert = Alert.find_or_initialize_by(
+      source: Source.find_or_initialize_by(slug: alert_params[:source]),
+      finding_id: alert_params[:finding_id],
+      target: alert_params[:target]
+    )
+
+    alert_params[:urls].map do |url|
+      alert.urls.find_or_initialize_by(name: url[:name], url: url[:url])
+    end
+
+    alert_params[:key_values].map do |kv|
+      alert.key_values.find_or_initialize_by(key: kv[:key], value: kv[:value])
+    end
+
+    alert.last_detected_at = alert_params[:detected_at]
+    alert.finding = alert_params[:finding]
+    alert.severity_external = Severity.friendly.find(alert_params[:severity])
+
+    if alert.persisted?
+      alert.count += 1
+    else
+      alert.name = alert_params[:name]
+      alert.detail = alert_params[:detail]
+      alert.notes = alert_params[:notes]
+      alert.first_detected_at = alert_params[:detected_at]
+      alert.kind = Kind.friendly.find(alert_params[:kind])
+      alert.family = Family.friendly.find(alert_params[:family])
+      alert.severity_internal = Severity.friendly.find(alert_params[:severity])
+    end
+
+    puts alert.inspect
+
+    alert.save!
+    render json: alert, serializer: Api::V1::AlertSerializer
+  end
+
+  private
+
+  def alert_params
+    params.permit(
+      :name,
+      :target,
+      :detail,
+      :notes,
+      :finding,
+      :finding_id,
+      :detected_at,
+      :kind,
+      :family,
+      :source,
+      :severity,
+      urls: [:name, :url],
+      key_values: [:key, :value]
+    )
   end
 
 end
