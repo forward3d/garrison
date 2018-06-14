6d8328e530e4dbde98bc779a723d4134f352df71
Lloyd Pick <lloydpick@gmail.com>
2018-06-14 08:13:23 -0400

    default to alerts#index for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: "alerts#index"
+
   resources :alerts do
     member do
       post :unverify
